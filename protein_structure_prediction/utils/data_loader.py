eb4bcf4b0b22c07fe3f642e90c63cc8bbd9d5ba0
zeoyzhaogithub <1401518843@qq.com>
2025-05-12 10:05:16 +0800

    [+]项目结构初始化

---
+++ protein_structure_prediction/utils/data_loader.py
@@ -0,0 +1,9 @@
+#! /user/bin/python3.7
+# coding: utf-8
+# @Time: 2025-04-19
+# @Author: 
+
+"""
+数据加载与特征工程
+用法：python 
+"""
---
+++ protein_structure_prediction/utils/download_data.py
@@ -0,0 +1,9 @@
+#! /user/bin/python3.7
+# coding: utf-8
+# @Time: 2025-04-21
+# @Author: 
+
+"""
+数据下载
+用法：python 
+"""
